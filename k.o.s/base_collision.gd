93a04ad4abea5213db88b274fab3eb495471a6db
zedbros <jameszed@icloud.com>
2026-03-21 20:06:02 +0100

    Revert "Started collision logic and structure"
    This reverts commit 42978bb2218c79f537638166d0c7493f4d980af8.

--- k.o.s/base_collision.gd
+++ k.o.s/base_collision.gd
@@ -1,6 +1,7 @@
-extends Node
-class_name Spell
+extends CollisionShape2D
 
+
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
