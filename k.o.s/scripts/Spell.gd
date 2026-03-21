42978bb2218c79f537638166d0c7493f4d980af8
zedbros <jameszed@icloud.com>
2026-03-21 19:36:10 +0100

    Started collision logic and structure

--- k.o.s/scripts/Spell.gd
+++ k.o.s/scripts/Spell.gd
@@ -1,7 +1,6 @@
-extends CollisionShape2D
+extends Node
+class_name Spell
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
