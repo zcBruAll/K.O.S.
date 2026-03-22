81a35c0c312cb9f0a5fee00e6168220210de6ad0
Zoatik <119761787+Zoatik@users.noreply.github.com>
2026-03-22 15:06:45 +0100

    update bg for spell

--- k.o.s/selectedSpellEffect.gd
+++ k.o.s/selectedSpellEffect.gd
@@ -1,14 +1,17 @@
 extends Node2D 
 class_name SelectedSpell
 
-@onready var anim_player = $Sprite2D2/AnimationPlayer
-@onready var spellSprite: Sprite2D = $Sprite2D2
+@onready var anim_player = $AnimationPlayer
+@onready var spellSprite: Sprite2D = $fg
 var type = ""
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	anim_player.play("idle")
+	
 	
 func play_tap_anim() -> void:
 	anim_player.play("hammer_tap")
+	
+
 	
 	
